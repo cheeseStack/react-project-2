50c776f44eea051e89cd79bd0fd1e788bfce5efa
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-06-07 22:14:05 +0100

    added dynamic data to display Trip List and added css styles in seperate css file

---
+++ tripago/src/components/TripList.js
@@ -0,0 +1,32 @@
+import { React, useState, useEffect} from 'react'
+
+// styles
+import './TripList.css'
+
+export default function TripList() {
+    const [trips, setTrips] = useState([])
+
+    useEffect(() => {   
+        fetch('http://localhost:3000/trips')
+        .then(response => response.json())
+        .then(json => setTrips(json))
+    }, [])
+
+    console.log(trips)
+
+  
+
+  return (
+    <div className='trip-list'>
+        <h2>TripList</h2>
+        <ul>
+            {trips.map(trip => (
+                <li key={trip.id}>
+                    <h3>{trip.title}</h3>
+                    <p>{trip.price}</p>
+                </li>
+            ))}
+        </ul>
+    </div>
+  )
+}
