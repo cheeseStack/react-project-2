de9fe1273d5235ff3cd2afa121e576b4a8b051a7
Keith <69634519+cheeseStack@users.noreply.github.com>
2022-06-30 22:20:27 +0100

    added clecnup ffunction

--- src/App.js
+++ src/App.js
@@ -5,12 +5,15 @@
 
 import './App.css';
 import TripList from './components/TripList';
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+  const [showTrips, setShowTrips] = useState(true)
+
   return (
     <div className="App">
-        <TripList/>
+        <button onClick={() => setShowTrips(false)}>Hide Events</button>
+        {showTrips && <TripList/>}
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -9,13 +9,15 @@ export const useFetch = (url) => {
 
 
     useEffect(() => {
+
+        const controller = new AbortController()
         const fetchData = async () => {
 
             // while data is being fetched, show the loading message / state
             setIsPending(true)
 
             try {
-                const res = await fetch(url)
+                const res = await fetch(url, { signal: controller.signal})
                 // if check to see if the "ok" status == true
                 if (!res.ok) {
                     throw new Error(res.statusText)
@@ -28,9 +30,14 @@ export const useFetch = (url) => {
                 setData(json)
                 setError(null)
             } catch (err) {
-                setIsPending(false)
-                setError('Could not fetch the data')
-                console.log(err.message);
+                if(err.name === "AbortError") {
+                    console.log('the fetch was aborted');
+                } else {
+                    setIsPending(false)
+                    setError('Could not fetch the data')
+                    console.log(err.message);
+                }
+               
             }
 
 
@@ -40,6 +47,12 @@ export const useFetch = (url) => {
             
         
         fetchData()
+
+        // aborting fetch requests: https://netninja.dev/courses/1546635/lectures/35907438
+        return () => {
+            controller.abort()
+
+        }
 
     }, [url]) // set the url as a depencdency so that it runs again if the url changes
 
